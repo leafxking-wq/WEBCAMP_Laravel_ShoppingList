92cb3838b500bdb346247c4dc9378f112797dd10
Rena <rena@example.com>
2025-11-14 11:44:28 +0000

    再提出１

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 @section('contents')
         <h1>ログイン</h1>
         @if (session('front.user_register_success') == true)
-            ユーザを登録しました！<br>
+            ユーザを登録しました！！<br>
         @endif
         @if ($errors->any())
             <div>
@@ -15,8 +15,8 @@
         @endif        
         <form action="/login" method="post">
             @csrf
-            email：<input type="text" name="email" value="{{ old('email') }}"><br>
-            パスワード：<input type="password" name="password"><br>
+            email:<input type="text" name="email" value="{{ old('email') }}"><br>
+            パスワード:<input type="password" name="password"><br>
             <button>ログインする</button>
         </form>
         <a href="/user/register">会員登録</a>
